af53ca3ed2b62cd1cf25fbd6fcf73eb5985c85ca
yanivhamou <yaniv.hamou@allcloud.io>
2021-08-26 09:35:26 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const  express = require ('express');
+const app = express() ;
+
+app.listen(3000,() =>{
+    console.log("dfind aws");
+
+});
+
+app.get('/', (req,res) => {
+    res.send("welcome ");
+
+});
